3c24866f00770d3e85719a0c8e140eb216683d56
Golubev Vasiliy <vase4ka346@gmail.com>
2015-08-29 01:51:25 +0300

    Преобразование строкового параметра в числовой (int)

--- src/resheto/Resheto.java
+++ src/resheto/Resheto.java
@@ -2,7 +2,9 @@ package resheto;
 public class Resheto {
 
     public static void main(String[] args) {
-        
+        int N;
+        N = Integer.parseInt(args[0]);
+        System.out.println(N);
     }
     
 }
