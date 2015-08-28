18315c844959b4dbf52b56b1d8ee5b3e42868ab3
Golubev Vasiliy <vase4ka346@gmail.com>
2015-08-29 01:59:08 +0300

    Заполнение простыми числами

--- src/resheto/Resheto.java
+++ src/resheto/Resheto.java
@@ -1,10 +1,17 @@
 package resheto;
+import java.util.ArrayList;
 public class Resheto {
 
     public static void main(String[] args) {
         int N;
         N = Integer.parseInt(args[0]);
         System.out.println(N);
+        ArrayList Res = new ArrayList();
+        for (int i=2; i<=N; i++)
+        {
+            Res.add(i);
+        }
+        System.out.print(Res);
     }
     
 }
