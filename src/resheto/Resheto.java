1aff49e2f0dc70767b98049bc2fa7acd8b3379f2
Golubev Vasiliy <vase4ka346@gmail.com>
2015-08-29 03:01:47 +0300

    Программа выводит все простые числа в промежутке от двух до N N - число передаваемое параметром

--- src/resheto/Resheto.java
+++ src/resheto/Resheto.java
@@ -3,13 +3,19 @@ import java.util.ArrayList;
 public class Resheto {
 
     public static void main(String[] args) {
-        int N;
+        int N, p, k=0;
         N = Integer.parseInt(args[0]);
-        System.out.println(N);
-        ArrayList Res = new ArrayList();
+        System.out.println("Простые числа в промежутке от 2 до " + N);
+        ArrayList<Integer> Res = new ArrayList();
         for (int i=2; i<=N; i++)
-        {
             Res.add(i);
+        while(Res.get(k)*Res.get(k)<N)
+        {
+            p=Res.get(k)*Res.get(k);
+            for (int i=Res.indexOf(p); i<Res.size(); i++)
+                if(Res.get(i) % Res.get(k) ==0)
+                    Res.remove(i);
+            k++;
         }
         System.out.print(Res);
     }
